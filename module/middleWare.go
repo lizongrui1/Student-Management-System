0c2077ac032e74e18bf8bd62e2e2bdbc0117098c
zongrui li <192292177@qq.com>
2024-02-16 20:45:53 +0800

    no message

--- module/middleWare.go
+++ module/middleWare.go
@@ -3,20 +3,38 @@ package module
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/redis/go-redis/v9"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 	"time"
 )
 
 //var tpl *template.Template
 
 func InitDB() (*sql.DB, error) {
+	Initconfig()
+	username := viper.GetString("database.username")
+	password := viper.GetString("database.password")
+	host := viper.GetString("database.host")
+	port := viper.GetString("database.port")
+	database := viper.GetString("database.name")
+	charset := viper.GetString("database.charset")
+
 	//tpl = template.Must(template.ParseGlob("./module/templates/*.html"))
-	db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/studb?charset=utf8")
+	//db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/studb?charset=utf8")
+	//if err != nil {
+	//	return nil, err
+	//}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s", username, password, host, port, database, charset)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
 	}
+
 	if err := db.Ping(); err != nil {
 		err := db.Close()
 		if err != nil {
@@ -46,4 +64,18 @@ func InitRDB() error {
 		log.Fatalf("无法连接到MySQL数据库: %v", err)
 	}
 	return nil
+}
+
+func Initconfig() {
+	workDir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("初始化配置文件失败，err：%s", err.Error())
+	}
+
+	viper.SetConfigName("database")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(workDir + "/config")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("读取配置文件失败，err：%s", err.Error())
+	}
 }
